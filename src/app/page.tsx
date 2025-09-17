4015974956ab77d1d780d95db1b5eb2585aa29ef
artartur <artur.souza@tds.company>
2025-09-16 21:27:17 -0300

    feat(home): add components to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import About from "./components/About";
+import Banner from "./components/Banner";
+import Contact from "./components/Contact";
+import Enterprises from "./components/Enterprises";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 export default function Home() {
   return (
     <div>
-
+      <Header />
+      <main>
+        <Banner />
+        <About />
+        <Contact />
+        <Enterprises />
+      </main>
+      <Footer />
     </div>
   );
 }
