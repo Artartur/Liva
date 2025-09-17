d78243ffeb7e546fb6bc6622825c098b51974427
artartur <artur.souza@tds.company>
2025-09-16 22:56:34 -0300

    feat(ui): create MenuIcons component

---
+++ src/app/components/ui/utils/MenuIcons.tsx
@@ -0,0 +1,37 @@
+
+export function CloseIcon() {
+  return (
+    <svg
+      className="xl:hidden"
+      width="17"
+      height="18"
+      viewBox="0 0 17 18"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_26_878)">
+        <path d="M15.0536 2.9087L14.3214 2.1405L8.5 8.25362L2.6786 2.1405L1.94641 2.9087L7.77297 9.0164L1.94641 15.1241L2.6786 15.8923L8.5 9.77919L14.3214 15.8923L15.0536 15.1241L9.22704 9.0164L15.0536 2.9087Z" fill="black" stroke="black" strokeWidth="1.4" />
+      </g>
+      <defs>
+        <clipPath id="clip0_26_878">
+          <rect width="16.5" height="17.3115" fill="white" transform="translate(0.25 0.360596)" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export function HamburgerIcon() {
+  return (
+    <svg
+      className="xl:hidden"
+      width="16"
+      height="12"
+      viewBox="0 0 16 12"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M16 0.305542H0V3.00028H16V0.305542Z" fill="#010002" />
+      <path d="M16 4.62085H0V7.31559H16V4.62085Z" fill="#010002" />
+      <path d="M16 8.99976H0V11.6945H16V8.99976Z" fill="#010002" />
+    </svg>
+  )
+}
