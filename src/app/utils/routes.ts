95e6338c0320cdd7538394dd24aacf6c6c34939a
artartur <artur.souza@tds.company>
2025-09-16 22:48:31 -0300

    feat(utils): define routes object

---
+++ src/app/utils/routes.ts
@@ -0,0 +1,8 @@
+
+export const navItems = [
+  { id: 'home', label: 'HOME' },
+  { id: 'about', label: 'SOBRE A LIVA' },
+  { id: 'enterprises', label: 'EMPREENDIMENTOS' },
+  { id: '', label: 'NOTÍCIAS' },
+  { id: 'contact', label: 'CONTATO' }
+];
